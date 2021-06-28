d82953b6de7e402fd6cac572ad9862fe1c497c35
Rishabh Kohale <r.kohale9@gmail.com>
2021-06-28 14:46:39 +0530

    gene crud operations added

--- modules/gene/server-ts/resolvers.ts
+++ modules/gene/server-ts/resolvers.ts
@@ -1,5 +1,115 @@
+import withAuth from 'graphql-auth';
+import { withFilter } from 'graphql-subscriptions';
+
+const GENE_SUBSCRIPTION = 'gene_subscription';
+
 export default (pubsub: any) => ({
-  Query: {},
-  Mutation: {},
-  Subscription: {}
+  Query: {
+    async genes(obj: any, { filter, limit, after, orderBy }: any, context: any) {
+      const geneOutput = await context.Gene.genes(limit, after, orderBy, filter);
+      const { geneItems, total } = geneOutput;
+      const hasNextPage = total > after + limit;
+
+      const edgesArray = [];
+      geneItems &&
+        geneItems.map((geneItem, index) => {
+          edgesArray.push({
+            cursor: after + index,
+            node: geneItem
+          });
+        });
+      const endCursor = edgesArray.length > 0 ? edgesArray[edgesArray.length - 1].cursor : 0;
+      return {
+        totalCount: total,
+        edges: edgesArray,
+        pageInfo: {
+          endCursor,
+          hasNextPage
+        }
+      };
+    },
+    async gene(obj: any, { id }: any, { Gene }: any) {
+      const gene = await Gene.gene(id);
+      return gene;
+    }
+  },
+  Mutation: {
+    addGene: withAuth(async (obj: any, { input }: any, context: any) => {
+      try {
+        // if (!input.userId) {
+        //   input.userId = context.req.identity.id;
+        // }
+        const ga = await context.Gene.addGene(input);
+        const gene = await context.Gene.gene(ga.id);
+        // publish for genes list
+        pubsub.publish(GENE_SUBSCRIPTION, {
+          geneUpdated: {
+            mutation: 'CREATED',
+            node: gene
+          }
+        });
+        return gene;
+      } catch (e) {
+        return e;
+      }
+    }),
+    editGene: withAuth(async (obj: any, { input }: any, context: any) => {
+      try {
+        await context.Gene.editGene(input);
+        const gene = await context.Gene.gene(input.id);
+        // publish for edit gene page
+        pubsub.publish(GENE_SUBSCRIPTION, {
+          geneUpdated: {
+            mutation: 'UPDATED',
+            node: gene
+          }
+        });
+        return gene;
+      } catch (e) {
+        return e;
+      }
+    }),
+    deleteGene: withAuth(async (obj: any, { id }: any, context: any) => {
+      try {
+        const gene = await context.Gene.gene(id);
+        const isDeleted = await context.Gene.deleteGene(id);
+        if (isDeleted) {
+          // publish for edit gene page
+          pubsub.publish(GENE_SUBSCRIPTION, {
+            geneUpdated: {
+              mutation: 'DELETED',
+              node: gene
+            }
+          });
+          return gene;
+        } else {
+          return null;
+        }
+      } catch (e) {
+        throw Error('Deleting Gene Failed');
+      }
+    })
+  },
+  Subscription: {
+    geneUpdated: {
+      subscribe: withFilter(
+        () => pubsub.asyncIterator(GENE_SUBSCRIPTION),
+        (payload, variables) => {
+          const { mutation, node } = payload.geneUpdated;
+          const {
+            filter: { searchText }
+          } = variables;
+          const checkByFilter = !searchText || node.title.toUpperCase().includes(searchText.toUpperCase());
+          switch (mutation) {
+            case 'DELETED':
+              return true;
+            case 'CREATED':
+              return checkByFilter;
+            case 'UPDATED':
+              return !checkByFilter;
+          }
+        }
+      )
+    }
+  }
 });
--- modules/gene/server-ts/sql.ts
+++ modules/gene/server-ts/sql.ts
@@ -1,7 +1,98 @@
-import { knex } from '@gqlapp/database-server-ts';
+import { Model, raw } from 'objection';
+import { camelizeKeys, decamelizeKeys, decamelize } from 'humps';
+import { knex, returnId, orderedFor } from '@gqlapp/database-server-ts';
+import { has } from 'lodash';
 
-export default class Gene {
-  public genes() {
-    return knex.select();
+// Give the knex object to objection.
+const eager = '[]';
+
+Model.knex(knex);
+
+export default class Gene extends Model {
+  static get tableName() {
+    return 'gene';
+  }
+
+  static get idColumn() {
+    return 'id';
+  }
+
+  public async genes(limit: number, after: any, orderBy: any, filter: any) {
+    const queryBuilder = Gene.query()
+      .withGraphFetched(eager)
+      .orderBy('id', 'desc');
+    if (orderBy && orderBy.column) {
+      const column = orderBy.column;
+      let order = 'asc';
+      if (orderBy.order) {
+        order = orderBy.order;
+      }
+
+      queryBuilder.orderBy(decamelize(column), order);
+    }
+
+    if (filter) {
+      if (has(filter, 'isActive') && filter.isActive !== '') {
+        queryBuilder.where(function() {
+          this.where('gene.is_active', filter.isActive);
+        });
+      }
+      if (has(filter, 'searchText') && filter.searchText !== '') {
+        queryBuilder
+          .from('gene')
+          .groupBy('gene.id')
+          .where(function() {
+            this.where(raw('LOWER(??) LIKE LOWER(?)', ['gene.title', `%${filter.searchText}%`])).orWhere(
+              raw('LOWER(??) LIKE LOWER(?)', ['gene.description', `%${filter.searchText}%`])
+            );
+          });
+      }
+    }
+
+    const allGeneItems = camelizeKeys(await queryBuilder);
+
+    const total = allGeneItems.length;
+    let res = {};
+
+    if (limit && after) {
+      res = camelizeKeys(
+        await queryBuilder
+          .limit(limit)
+          .offset(after)
+          .groupBy('gene.id')
+      );
+    } else if (limit && !after) {
+      res = camelizeKeys(await queryBuilder.limit(limit).groupBy('gene.id'));
+    } else if (!limit && after) {
+      res = camelizeKeys(await queryBuilder.offset(after).groupBy('gene.id'));
+    } else {
+      res = camelizeKeys(await queryBuilder.groupBy('gene.id'));
+    }
+    return { geneItems: res, total };
+  }
+  public async gene(id: number) {
+    // const res =
+    return camelizeKeys(
+      await Gene.query()
+        .findById(id)
+        .withGraphFetched(eager)
+        .orderBy('id', 'desc')
+    );
+  }
+
+  public async addGene(params: any) {
+    const res = await Gene.query().insertGraph(decamelizeKeys(params));
+    return res;
+  }
+
+  public async editGene(params: any) {
+    const res = await Gene.query().upsertGraph(decamelizeKeys(params));
+    return res;
+  }
+
+  public deleteGene(id: number) {
+    return knex('gene')
+      .where('id', '=', id)
+      .del();
   }
 }
