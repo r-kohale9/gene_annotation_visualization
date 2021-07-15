e49387a9254f4e2aff729d9c2d7d62b21d53ed52
Rishabh Kohale <r.kohale9@gmail.com>
2021-07-15 23:30:29 +0530

    pathways query muation subscriptions added

--- modules/pathway/server-ts/resolvers.ts
+++ modules/pathway/server-ts/resolvers.ts
@@ -1,5 +1,115 @@
+import withAuth from 'graphql-auth';
+import { withFilter } from 'graphql-subscriptions';
+
+const PATHWAY_SUBSCRIPTION = 'pathway_subscription';
+
 export default (pubsub: any) => ({
-  Query: {},
-  Mutation: {},
-  Subscription: {}
+  Query: {
+    async pathways(obj: any, { filter, limit, after, orderBy }: any, context: any) {
+      const pathwayOutput = await context.Pathway.pathways(limit, after, orderBy, filter);
+      const { pathwayItems, total } = pathwayOutput;
+      const hasNextPage = total > after + limit;
+
+      const edgesArray = [];
+      pathwayItems &&
+        pathwayItems.map((pathwayItem, index) => {
+          edgesArray.push({
+            cursor: after + index,
+            node: pathwayItem
+          });
+        });
+      const endCursor = edgesArray.length > 0 ? edgesArray[edgesArray.length - 1].cursor : 0;
+      return {
+        totalCount: total,
+        edges: edgesArray,
+        pageInfo: {
+          endCursor,
+          hasNextPage
+        }
+      };
+    },
+    async pathway(obj: any, { id }: any, { Pathway }: any) {
+      const pathway = await Pathway.pathway(id);
+      return pathway;
+    }
+  },
+  Mutation: {
+    addPathway: withAuth(async (obj: any, { input }: any, context: any) => {
+      try {
+        // if (!input.userId) {
+        //   input.userId = context.req.identity.id;
+        // }
+        const ga = await context.Pathway.addPathway(input);
+        const pathway = await context.Pathway.pathway(ga.id);
+        // publish for pathways list
+        pubsub.publish(PATHWAY_SUBSCRIPTION, {
+          pathwayUpdated: {
+            mutation: 'CREATED',
+            node: pathway
+          }
+        });
+        return pathway;
+      } catch (e) {
+        return e;
+      }
+    }),
+    editPathway: withAuth(async (obj: any, { input }: any, context: any) => {
+      try {
+        await context.Pathway.editPathway(input);
+        const pathway = await context.Pathway.pathway(input.id);
+        // publish for edit pathway page
+        pubsub.publish(PATHWAY_SUBSCRIPTION, {
+          pathwayUpdated: {
+            mutation: 'UPDATED',
+            node: pathway
+          }
+        });
+        return pathway;
+      } catch (e) {
+        return e;
+      }
+    }),
+    deletePathway: withAuth(async (obj: any, { id }: any, context: any) => {
+      try {
+        const pathway = await context.Pathway.pathway(id);
+        const isDeleted = await context.Pathway.deletePathway(id);
+        if (isDeleted) {
+          // publish for edit pathway page
+          pubsub.publish(PATHWAY_SUBSCRIPTION, {
+            pathwayUpdated: {
+              mutation: 'DELETED',
+              node: pathway
+            }
+          });
+          return pathway;
+        } else {
+          return null;
+        }
+      } catch (e) {
+        throw Error('Deleting Pathway Failed');
+      }
+    })
+  },
+  Subscription: {
+    pathwayUpdated: {
+      subscribe: withFilter(
+        () => pubsub.asyncIterator(PATHWAY_SUBSCRIPTION),
+        (payload, variables) => {
+          const { mutation, node } = payload.pathwayUpdated;
+          const {
+            filter: { searchText }
+          } = variables;
+          const checkByFilter = !searchText || node.title.toUpperCase().includes(searchText.toUpperCase());
+          switch (mutation) {
+            case 'DELETED':
+              return true;
+            case 'CREATED':
+              return checkByFilter;
+            case 'UPDATED':
+              return !checkByFilter;
+          }
+        }
+      )
+    }
+  }
 });
--- modules/pathway/server-ts/sql.ts
+++ modules/pathway/server-ts/sql.ts
@@ -1,7 +1,134 @@
+import { Model, raw } from 'objection';
+import { camelizeKeys, decamelizeKeys, decamelize } from 'humps';
 import { knex } from '@gqlapp/database-server-ts';
+import { has } from 'lodash';
 
-export default class Pathway {
-  public pathways() {
-    return knex.select();
+// Give the knex object to objection.
+const eager = '[]';
+
+Model.knex(knex);
+
+export default class Pathway extends Model {
+  static get tableName() {
+    return 'pathway';
+  }
+
+  static get idColumn() {
+    return 'id';
+  }
+
+  static get relationMappings() {
+    return {};
+  }
+
+  public async pathways(limit: number, after: any, orderBy: any, filter: any) {
+    const queryBuilder = Pathway.query()
+      .withGraphFetched(eager)
+      .orderBy('id', 'desc');
+    if (orderBy && orderBy.column) {
+      const column = orderBy.column;
+      let order = 'asc';
+      if (orderBy.order) {
+        order = orderBy.order;
+      }
+
+      queryBuilder.orderBy(decamelize(column), order);
+    }
+
+    if (filter) {
+      if (has(filter, 'isActive') && filter.isActive !== '') {
+        queryBuilder.where(function() {
+          this.where('pathway.is_active', filter.isActive);
+        });
+      }
+      if (has(filter, 'pathwayId') && filter.pathwayId !== '') {
+        queryBuilder.where(function() {
+          this.where('pathway.pathway_id', filter.pathwayId);
+        });
+      }
+      if (has(filter, 'pathwaySymbol') && filter.pathwaySymbol !== '') {
+        queryBuilder.where(function() {
+          this.where('pathway.pathway_symbol', filter.pathwaySymbol);
+        });
+      }
+      if (has(filter, 'pathwayName') && filter.pathwayName !== '') {
+        queryBuilder.where(function() {
+          this.where('pathway.pathway_name', filter.pathwayName);
+        });
+      }
+      if (has(filter, 'searchText') && filter.searchText !== '') {
+        queryBuilder
+          .from('pathway')
+          .groupBy('pathway.id')
+          .where(function() {
+            this.where(raw('LOWER(??) LIKE LOWER(?)', ['pathway.pathway_symbol', `%${filter.searchText}%`])).orWhere(
+              raw('LOWER(??) LIKE LOWER(?)', ['pathway.pathway_name', `%${filter.searchText}%`])
+            );
+          });
+      }
+    }
+
+    const allPathwayItems = camelizeKeys(await queryBuilder);
+
+    const total = allPathwayItems.length;
+    let res = {};
+
+    if (limit && after) {
+      res = camelizeKeys(
+        await queryBuilder
+          .limit(limit)
+          .offset(after)
+          .groupBy('pathway.id')
+      );
+    } else if (limit && !after) {
+      res = camelizeKeys(await queryBuilder.limit(limit).groupBy('pathway.id'));
+    } else if (!limit && after) {
+      res = camelizeKeys(await queryBuilder.offset(after).groupBy('pathway.id'));
+    } else {
+      res = camelizeKeys(await queryBuilder.groupBy('pathway.id'));
+    }
+    return { pathwayItems: res, total };
+  }
+  public async pathway(id: number, pathwayId: number, pathwaySymbol: number) {
+    let res = {};
+    if (id) {
+      res = camelizeKeys(
+        await Pathway.query()
+          .findById(id)
+          .withGraphFetched(eager)
+          .orderBy('id', 'desc')
+      );
+    } else if (pathwayId) {
+      res = camelizeKeys(
+        await Pathway.query()
+          .withGraphFetched(eager)
+          .where('pathway_id', pathwayId)
+          .orderBy('id', 'desc')
+      )[0];
+    } else if (pathwaySymbol) {
+      res = camelizeKeys(
+        await Pathway.query()
+          .withGraphFetched(eager)
+          .where('pathway_symbol', pathwaySymbol)
+          .orderBy('id', 'desc')
+      )[0];
+    }
+    return res;
+  }
+
+  public async addPathway(params: any) {
+    const res = await Pathway.query().insertGraph(decamelizeKeys(params));
+    return res;
+  }
+
+  public async editPathway(params: any) {
+    const res = await Pathway.query().upsertGraph(decamelizeKeys(params));
+    return res;
+  }
+
+  public deletePathway(id: number) {
+    return knex('pathway')
+      .where('id', '=', id)
+      .del();
   }
 }
