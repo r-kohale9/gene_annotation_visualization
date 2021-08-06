06b6a452fd70c02f56cd245406f78b372a8e20b1
r-kohale9 <r.kohale9@gmail.com>
2021-08-07 01:59:48 +0530

    tissue seeds added

---
+++ modules/cell/server-ts/seed-data/013_tissue.js
@@ -0,0 +1,46 @@
+export default [
+  {
+    tissue_type: 'Fetal gonad',
+    gene_id: '23019'
+  },
+  {
+    tissue_type: 'Umbilical cord blood',
+    gene_id: '23019'
+  },
+  {
+    tissue_type: 'Fetal kidney',
+    gene_id: '23019'
+  },
+  {
+    tissue_type: 'Liver',
+    gene_id: '1108'
+  },
+  {
+    tissue_type: 'Fetal kidney',
+    gene_id: '1108'
+  },
+  {
+    tissue_type: 'Hippocampus',
+    gene_id: '3148'
+  },
+  {
+    tissue_type: 'Embryonic prefrontal cortex',
+    gene_id: '3148'
+  },
+  {
+    tissue_type: 'Pancreatic islet',
+    gene_id: '3148'
+  },
+  {
+    tissue_type: 'Fetal kidney',
+    gene_id: '3148'
+  },
+  {
+    tissue_type: 'Lung',
+    gene_id: '23639'
+  },
+  {
+    tissue_type: 'Fetal gonad',
+    gene_id: '23066'
+  }
+];
--- modules/cell/server-ts/seeds/009_cell.js
+++ modules/cell/server-ts/seeds/009_cell.js
@@ -1,12 +1,18 @@
 import { returnId, truncateTables } from '@gqlapp/database-server-ts';
 import CELL from '../seed-data/009_cell';
+import TISSUE from '../seed-data/013_tissue';
 
 // eslint-disable-next-line import/prefer-default-export
 export async function seed(knex, Promise) {
-  await truncateTables(knex, Promise, ['cell']);
+  await truncateTables(knex, Promise, ['cell', 'tissue']);
   await Promise.all(
     CELL.map(async (ii, i) => {
       await returnId(knex('cell')).insert(ii);
+    })
+  );
+  await Promise.all(
+    TISSUE.map(async (ii, i) => {
+      await returnId(knex('tissue')).insert(ii);
     })
   );
 }
